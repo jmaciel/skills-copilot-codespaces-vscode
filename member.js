3f8e11b7fa3255cfba8609d221fe79fbc965982e
João Maciel <j.maciel.hd@gmail.com>
2024-01-11 18:01:15 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember() {
+  var skills = ["JavaScript", "HTML", "CSS"];
+  var totalSkills = skills.length;
+  var message = "You have " + totalSkills + " skills";
+  console.log(message);
+}
